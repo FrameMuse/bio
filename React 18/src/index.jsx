be7dc2994709e9e76024d13254509dfedfbc736b
FrameMuse <minicablestone@gmail.com>
2021-11-01 02:53:17 +0300

    Start Transition

--- React 18/src/index.jsx
+++ React 18/src/index.jsx
@@ -1,12 +1,19 @@
-import ReactDom from "react-dom"
+import ReactDOM from "react-dom"
 
 import AutomaticBatching from "./features/Automatic Batching"
 import StartTransition from "./features/Start Transition"
-
-ReactDom.render(
-  <div>
-    <AutomaticBatching />
-    <StartTransition />
-  </div>,
-  document.getElementById("root")
-)
+ReactDOM
+  .createRoot(document.getElementById("root"))
+  .render(
+    <div>
+      <AutomaticBatching />
+      <br />
+      <br />
+      <br />
+      <StartTransition />
+      <br />
+      <br />
+      <br />
+      <StartTransition useNonBlockingInput />
+    </div>
+  )
